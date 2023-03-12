a788d88a3de160833079ca17dab4d6ceec25b33b
CWIN77 <cwstbp0426@gmail.com>
2023-03-12 20:40:07 +0900

    size 변경

--- src/components/design/styleEditor/size.tsx
+++ src/components/design/styleEditor/size.tsx
@@ -81,7 +81,6 @@ function ExpandSize({ text, value }: { text: string, value: string }) {
     return { ...total.props, onKeyDown, onBlur, disabled: expand }
   }
 
-
   return (
     <SizeGroup2>
       <SizeGroup3 state={String(expand)}>
